330d98dc358f09ad1dc189ab838628a40a11daf3
JulianPagliaro <julian.gpagliaro@gmail.com>
2025-10-14 21:04:14 -0300

    Faltan agregar 2 Controllers.

---
+++ Solution/Mapping/EditorMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GameStore.Application.Dtos.Editor;
+using GameStore.Entities;
+
+namespace GameStore.WebAPI.Mapping
+{
+    public class EditorMappingProfile : Profile
+    {
+        public EditorMappingProfile()
+        {
+            CreateMap<Editor, EditorResponseDto>().
+                ForMember(dest=>dest.FechaFundacion, ori=>ori.MapFrom(src=>src.FechaFundacion.ToShortDateString()));
+           
+            CreateMap<EditorRequestDto, Editor>();
+        }
+    }
+}
---
+++ Solution/Mapping/GeneroMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GameStore.Application.Dtos.Genero;
+using GameStore.Entities;
+
+namespace GameStore.WebAPI.Mapping
+{
+    public class GeneroMappingProfile : Profile
+    {
+        public GeneroMappingProfile()
+        {
+            CreateMap<Genero, GeneroResponseDto>();
+            CreateMap<GeneroRequestDto, Genero>();
+        }
+    }
+}
